25f08ba0543e322143154a65731c1d6682d7f71d
Elena Petrova <elena.i.11@yandex.ru>
2022-08-10 22:15:49 +0400

    task13

---
+++ task15/task13/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введи число: ");
+int Number = int.Parse(Console.ReadLine());
+if (Number > 99)
+    Console.WriteLine((Number / 100) % 10);
+else
+    Console.WriteLine("-> третьей цифры нет");
